0b35de57385488b06ddabf662b0cf0e93e60a997
Gabriel <gabrielbtegani@gmail.com>
2024-04-30 08:43:08 -0300

    Refactor routing logic and add comments for better readability

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
+// Importa a classe Router do arquivo "routes.js"
 import { Router } from "./routes.js";
 
+// Cria uma instância da classe Router
 const routeNav = new Router();
-routeNav.add('/', '../pages/home.html');
-routeNav.add('/about', '../pages/about.html');
-routeNav.add('/contact', '../pages/contact.html');
-routeNav.add(404, '../pages/404.html');
+
+// Adiciona rotas e suas páginas correspondentes
+routeNav.add('/', '../pages/home.html');      // Rota raiz redirecionada para home.html
+routeNav.add('/about', '../pages/about.html'); // Rota '/about' redirecionada para about.html
+routeNav.add('/contact', '../pages/contact.html'); // Rota '/contact' redirecionada para contact.html
+routeNav.add(404, '../pages/404.html');       // Rota de erro 404 redirecionada para 404.html
+
+// Exibe no console o objeto routeNav, que contém as rotas e páginas correspondentes
 console.log(routeNav);
 
+// Chama o método handle para lidar com a rota atual quando o script é carregado
 routeNav.handle();
 
+// Define um evento para lidar com o botão "voltar" do navegador
 window.onpopstate = () => routeNav.handle();
+
+// Define uma função global 'route' para lidar com a navegação para uma nova rota
 window.route = () => routeNav.route();
--- scripts/routes.js
+++ scripts/routes.js
@@ -1,27 +1,43 @@
+// Definição da classe Router
 class Router {
+   // Propriedade para armazenar as rotas
    routes = {};
    
+   // Método para adicionar uma rota
    add(routeName, page) {
       this.routes[routeName] = page;
    };
 
+   // Método para lidar com a navegação para uma rota
    route(event) {
+      // Se o evento não estiver definido, usa o evento global do navegador
       event = event || window.event;
+      // Impede o comportamento padrão do evento, como o carregamento de uma nova página
       event.preventDefault();
 
+      // Atualiza o histórico de navegação do navegador para refletir a nova rota
       window.history.pushState({}, "", event.target.href);
+      // Chama o método handle para lidar com a nova rota
       this.handle();
    };
    
+   // Método para lidar com a rota atual
    handle() {
-      // forma de desestruturar um objeto de uma propriedade, podendo pegar varios
+      // Obtém o pathname da URL atual
       const { pathname } = window.location;
+      // Obtém a rota correspondente com base no pathname ou a rota 404 se não houver correspondência
       const route = this.routes[pathname] || this.routes[404];
    
-      fetch(route).then(data => data.text()).then(html => {
-         document.querySelector('#app').innerHTML = html;
-      });
+      // Faz uma solicitação fetch para obter o conteúdo da página da rota
+      fetch(route)
+         // Converte a resposta em texto
+         .then(data => data.text())
+         // Manipula o HTML retornado e o insere no elemento com o ID 'app'
+         .then(html => {
+            document.querySelector('#app').innerHTML = html;
+         });
    };
-}
+};
 
-export{ Router };
+// Exporta a classe Router
+export { Router };
